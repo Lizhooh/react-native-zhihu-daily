f8f88aaa5543fb2fa1bd4325203c912d906aa2eb
Lizhooh <960316408@qq.com>
2017-02-09 23:50:33 +0800

    add net api

---
+++ App/Server/api.js
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import ReactNative from 'react-native';
+
+const version = 4;
+const protocol = 'http';
+
+const themes = {
+    url: `${protocol}://news-at.zhihu.com/api/${version}/themes`,
+    get() {
+        return fetch(this.url)
+            .then(res => res.json())
+            .then(jsondata => {
+                return jsondata;
+            })
+            .catch(err => console.error("themes api Error: " + err))
+    },
+};
+
+const latest = {
+    url: `${protocol}://news-at.zhihu.com/api/${version}/stories/latest`,
+    get() {
+        return fetch(this.url)
+            .then(res => res.json())
+            .then(jsondata => {
+                return jsondata;
+            })
+            .catch(err => console.error("latest api Error: " + err))
+    }
+};
+
+const theme = {
+    url: `${protocol}://news-at.zhihu.com/api/${version}/theme/`,
+    get(id) {
+        return fetch(this.url + id)
+            .then(res => res.json())
+            .then(jsondata => {
+                return jsondata;
+            })
+            .catch(err => console.error("theme api Error: " + err))
+    }
+};
+
+const Api = {
+    themes,
+    latest,
+    theme,
+}
+
+export default Api;
+

f7308762af81487fcf1eb882af98ed0c6faafc1e
Lizhooh <960316408@qq.com>
2017-08-10 11:39:31 +0800

    update package

--- android/app/src/main/java/com/zhihudaily/MainApplication.java
+++ android/app/src/main/java/com/zhihudaily/MainApplication.java
@@ -3,6 +3,7 @@ package com.zhihudaily;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.yamill.orientation.OrientationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OrientationPackage(),
             new VectorIconsPackage(),
             new RNFetchBlobPackage()
       );
